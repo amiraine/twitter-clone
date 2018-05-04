aab1384630cdbc35d6379d37459771cb542a74db
Ami cooper <amiraine.cooper@gmail.com>
2018-05-04 15:44:16 -0700

    make new tweets prepend to timeline

--- src/app/timeline/timeline.component.ts
+++ src/app/timeline/timeline.component.ts
@@ -14,12 +14,13 @@ export class TimelineComponent implements OnInit {
   tweets: Tweet[] = [
     new Tweet('bob','@bob','I need more coffee #TGIF',0,3,5),
 
-    new Tweet('Trillary Clinton','@gaymergirl','Lorem ipsum dolor amet palo santo la croix paleo coloring book portland deep v literally forage copper mug taiyaki.',1,3,2),
+    new Tweet('Trillary Clinton','@gaymergirl','TFW you pass your code review on the first try <img src="https://img.buzzfeed.com/buzzfeed-static/static/enhanced/terminal05/2012/6/2/10/anigif_enhanced-buzz-25833-1338648475-5.gif">',1,3,2),
 
-    new Tweet('russianspambot','@cyka','You probably haven\'t heard of them beard prism literally squid.',2,2,10)
+    new Tweet('russian spam bot','@cyka','You probably haven\'t heard of them beard prism literally squid.',2,2,10)
   ];
   makeTweet(tweet){
     let newTweet = new Tweet('name','@handle',tweet,0,0,0);
     this.tweets.splice(0,0,newTweet);
   }
+
 }
