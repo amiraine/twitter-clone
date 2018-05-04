f004b04d36c7b5777b9df75e622c40306ae9d992
Ami cooper <amiraine.cooper@gmail.com>
2018-05-04 08:59:24 -0700

    create HTML template which contains the components, add some css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { UserInfoComponent } from './user-info/user-info.component';
@@ -16,7 +16,8 @@ import { TimelineComponent } from './timeline/timeline.component';
     TimelineComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
