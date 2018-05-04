0d2b032f33f1d09a8b8f5badca9bb391fd30d3a0
Ami cooper <amiraine.cooper@gmail.com>
2018-05-04 10:45:36 -0700

    try to populate timeline with hard coded tweets

--- src/app/models/tweet.model.ts
+++ src/app/models/tweet.model.ts
@@ -2,5 +2,5 @@ export class Tweet {
   public replies: number = 0;
   public retweets: number = 0;
   public likes: number = 0;
-  constructor(public tweetContent: string){ }
+  constructor(public displayName:string, public atName:string,public tweetContent: string){ }
 }
